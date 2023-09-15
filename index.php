5896cfcb85dd32ee99648b303df6e9a3e1c0fb37
Abhishek Bhadoriya <88373208+Abhi98Singh@users.noreply.github.com>
2023-09-15 15:40:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -201,6 +201,6 @@ Badi-Gorela-Mulla Talai Rd, Haridas Ji Ki Magri, Pichola, Udaipur, Rajasthan 313
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-//
+
 </body>
 </html>
